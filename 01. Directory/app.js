1b5010691ea1de6b28f3958dbd59248f4ae4530c
dosoung <soungdo0919@gmail.com>
2020-03-25 23:19:08 +0900

    01

---
+++ 01. Directory/app.js
@@ -0,0 +1,55 @@
+console.log('app.js');
+
+// function log(str) {
+//   console.log(str);
+// };
+
+// var log = function(str) {
+//   console.log(str);
+// };
+
+// // log('hello')
+
+// var image ={
+//   width: 100,
+//   height: 100,
+//   name: '1.png'
+// };
+
+// var image1 = [1,2,3,4,5,6,'a',{}];
+// image1[0] = 'a';
+
+// var data = null;
+// var data1 = '';
+// var data2;
+
+// var imageWidth = 100;
+// var imageHeight = 100;
+// var imageName = '1.png';
+
+// console.log(image);
+
+// console.log(1+2);
+// console.log('a'+'b');
+
+// var count =1;
+// var number ='2';
+// console.log(count+number);
+
+// console.log(1 == 1);
+// console.log('' = '0');
+// console.log('' === 0)
+
+// var isHero = true;
+
+// if(isHero) {
+//   console.log('hero')
+// }
+
+// if(true) {
+//   console.log(1);
+// };
+
+// console.log(tyoeof(1));
+// console.log(typeof([]));
+// console.log(tyoeof(''));
