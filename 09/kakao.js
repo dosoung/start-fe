87161ae9d08a7d4f1c49deb3656d379c4c523189
dosoung <soungdo0919@gmail.com>
2020-05-26 15:29:01 +0900

    09

---
+++ 09/kakao.js
@@ -0,0 +1,37 @@
+const $btn = document.getElementById("search-btn");
+const $text = document.getElementById("search-text");
+const $result = document.getElementById("result");
+
+const url = "https://dapi.kakao.com/v2/search/web";
+
+const headers = { Authorization: "KakaoAK 48f6c33e9d8b2faf63ec6461c1a1b70a" };
+
+function success(data) {
+  const { documents } = data;
+  const li = documents.map((doc) => {
+    return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
+  });
+  $result.innerHTML = `<ul class="list-group list-group-flush>${li.join(
+    "",
+  )}</ul>`;
+}
+
+function error() {
+  alert("데이터를 읽어오지 못했습니다.");
+}
+
+function search() {
+  const { value } = $text;
+  if (value === "") {
+    alert("검색어를 입력하세요");
+    return false;
+  }
+
+  fetch(`${url}?query={value}`, { headers: headers })
+    .then((result) => result.json())
+    .then(success)
+    .catch(error);
+
+  return true;
+}
+$btn.addEventListener("click", search);
