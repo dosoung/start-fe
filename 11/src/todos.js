e637cc942eb1708fab16796291bc6b0d2f74a8fc
dosoung <soungdo0919@gmail.com>
2020-06-08 16:55:06 +0900

    done

---
+++ 11/src/todos.js
@@ -0,0 +1,23 @@
+import "./todos.css";
+import { render } from "./html-render";
+import formInput from "./form-input";
+
+const $result = document.querySelector("#result");
+const todos = [];
+
+$result.addEventListener("click", (event) => {
+  console.log(event.target.className);
+  const { className } = event.target;
+  if (className === "delete") {
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (className === "toggle-check") {
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone;
+    render(todos);
+  }
+});
+
+formInput.init(todos);
+render(todos);
---
+++ 11/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/todos.js",
+  output: {
+    filename: "app.js",
+    path: `${__dirname}/dist`,
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
