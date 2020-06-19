0992f76926d59fe8b4039be4aec8ede7876c5098
dosoung <soungdo0919@gmail.com>
2020-06-19 16:59:01 +0900

    count

--- 13/todo/src/App.js
+++ 13/todo/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
 
 function App() {
-  return <div className="App">HelloDohyunaaa</div>;
+  const [countNumber, setCounNumber] = useState(0);
+
+  function addCountNumber() {
+    setCounNumber(countNumber + 1);
+  }
+  return (
+    <div className="App">
+      <Header />
+      <div>{countNumber}</div>
+      <button onClick={addCountNumber}>플러스</button>
+    </div>
+  );
 }
 
 export default App;
